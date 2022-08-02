c8d583f1c17bca372ba358ae8a0a9976b2e35bb1
emmadal <emmanueldalougou@gmail.com>
2022-08-02 20:00:36 +0000

    refactor tab in profile page

---
+++ src/views/profile/resume.js
@@ -0,0 +1,42 @@
+// @mui material components
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+
+// Overview page components
+import PlatformSettings from "views/profile/components/PlatformSettings";
+import ProfileInfoCard from "examples/Cards/InfoCards/ProfileInfoCard";
+
+// Material Dashboard 2 React contexts
+import { useMaterialUIController } from "context";
+
+function Resume() {
+  const [controller] = useMaterialUIController();
+  const { userProfile } = controller;
+  return (
+    <MDBox mt={5} mb={3}>
+      <Grid container spacing={1}>
+        <Grid item xs={12} md={6} xl={6}>
+          <PlatformSettings />
+        </Grid>
+        <Grid item xs={12} md={6} xl={6} sx={{ display: "flex" }}>
+          <Divider orientation="vertical" sx={{ ml: -2, mr: 1 }} />
+          <ProfileInfoCard
+            title="Résume du profil"
+            description={userProfile?.description}
+            info={{
+              email: userProfile?.email,
+              contact: userProfile?.user_mobile_1,
+            }}
+            action={{ route: "", tooltip: "Modifier le profil" }}
+            shadow={false}
+          />
+        </Grid>
+      </Grid>
+    </MDBox>
+  );
+}
+
+export default Resume;
